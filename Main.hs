5e17fe85c408849e61d1e7b5c09a116e1a2c5add
Sampsa Kiiskinen <tuplanolla@gmail.com>
2015-07-23 02:00:11 +0300

    Files are complicated.

--- Main.hs
+++ Main.hs
@@ -1,18 +1,20 @@
 {-# LANGUAGE DeriveDataTypeable, OverloadedStrings, ScopedTypeVariables #-}
 
+-- import Control.Arrow (first, second)
 import Control.Applicative hiding (many, optional)
 import Control.Exception hiding (try)
 import Control.Monad
 import Data.Map hiding (filter, partition)
-import Data.List (isPrefixOf, partition)
+import Data.List (isPrefixOf)
 -- import Data.Text (Text)
--- import qualified Data.Text as T
+import qualified Data.Text as T (justifyLeft, pack, unpack)
 -- import qualified Data.Text.IO as TIO
 import Data.Typeable
 import Prelude hiding (foldl, getContents)
 -- import System.Exit (exitFailure, exitSuccess)
 import System.IO hiding (getContents)
 import System.IO.Error
+import System.Directory
 import System.Exit
 import System.Environment
 import System.Process (rawSystem)
@@ -81,22 +83,6 @@ parseActionsWith _ _ = Right []
 parseActions :: [String] -> Either CommandError [Action]
 parseActions = parseActionsWith Nothing
 
-fromString :: String -> Map String String
-fromString = fromList . fmap (partition (== ' ')) . lines
--- Parsec here.
-
-toString :: Map String String -> String
-toString = unlines . fmap (\ (x, y) -> x ++ "  " ++ y) . toAscList
-
-data ExecutionError =
-  AlreadyExists |
-  NoEditor |
-  EditorFailed Int |
-  Fuck
-  deriving (Show, Typeable)
-
-instance Exception ExecutionError
-
 data Action =
   Make |
   Edit |
@@ -112,19 +98,45 @@ data Action =
   deriving Show
 
 separatorParser :: Parser ()
-separatorParser = () <$ (space <* many1 space)
-
-continuationParser :: Parser ()
-continuationParser = () <$ many1 space
+separatorParser = () <$ char ' ' <* many1 (char ' ')
 
 fileParser :: Parser (Map String String)
 fileParser =
   fromList <$> many ((,) <$>
     (manyTill anyChar $ try $ separatorParser) <*>
-    (manyTill anyChar $ try $ eof P.<|> () <$ (newline <* notFollowedBy (try continuationParser))))
+    (manyTill anyChar $ try $ eof P.<|> () <$
+    (newline <* notFollowedBy (() <$ try newline <|> try separatorParser))))
 
 parseContents :: String -> Either ParseError (Map String String)
 parseContents = runParser fileParser () []
+
+-- This sucks.
+sanitizeContents :: Map String String -> Map String String
+sanitizeContents = mapBoth $ filter (/= '\n')
+
+justifyLeft :: Int -> Char -> String -> String
+justifyLeft n c = T.unpack . T.justifyLeft n c . T.pack
+
+mapBoth :: Ord b => (a -> b) -> Map a a -> Map b b
+mapBoth f = mapKeys f . Data.Map.map f
+
+formatContents :: Map String String -> String
+formatContents m =
+  let xs = toAscList $ sanitizeContents m
+      n = maximum $ length . fst <$> xs in
+      unlines $ (\ (x, y) -> justifyLeft n ' ' x ++ "  " ++ y) <$> xs
+
+data ExecutionError =
+  AlreadyExists |
+  NoEditor |
+  EditorFailed {exitCode :: Int} |
+  SyntaxError {row :: Int, column :: Int} |
+  ContentError {commandError :: ParseError} |
+  SwapInUse |
+  Fuck
+  deriving (Show, Typeable)
+
+instance Exception ExecutionError
 
 executeOne :: Configuration -> Action -> IO ()
 executeOne c Make =
@@ -136,23 +148,15 @@ executeOne c Make =
 executeOne c Edit =
   do m <- (<|>) <$> return (editor c) <*> lookupEnv "EDITOR" -- Check this.
      case m of
-          Just x -> do e <- rawSystem x [name c]
-                       case e of
-                            ExitFailure n -> throw $ EditorFailed n
-                            _ -> return ()
+          Just x ->
+            do e <- rawSystem x [name c]
+               case e of
+                    ExitFailure n -> throw $ EditorFailed n
+                    _ -> return ()
           _ -> throw NoEditor
-executeOne c (Add k v) =
-  do x <- readFile $ name c
-     _ <- evaluate $ length x
-     let y = toString $ insert k v $ fromString x
-     writeFile (name c) y
-     -- Write into swap file instead.
-executeOne c (Remove k) =
-  do x <- readFile $ name c
-     _ <- evaluate $ length x
-     let y = toString $ delete k $ fromString x
-     writeFile (name c) y
+executeOne c (Add k v) = changeContents c $ insert k v
 -- Not implemented yet.
+executeOne c (Remove k) = (c, k) `seq` undefined
 executeOne c (Update k v) = (c, k, v) `seq` undefined
 executeOne c (Lookup k) = (c, k) `seq` undefined
 executeOne c (Find k) = (c, k) `seq` undefined
@@ -161,24 +165,100 @@ executeOne c Destroy = c `seq` undefined
 executeOne c Help = c `seq` undefined
 executeOne c Version = c `seq` undefined
 
+changeContents :: Configuration -> (Map String String -> Map String String) -> IO ()
+changeContents c @ Configuration {name = file} f =
+  do x <- readFile file
+     _ <- evaluate $ length x
+     case parseContents x of
+          Right y ->
+            do fp <- getCurrentDirectory
+               fps <- getDirectoryContents fp
+               -- Here goes something to merge reality with expectations.
+               -- [FilePath] -> Map String String -> Map String (Maybe String, Bool)
+               -- That Maybe says whether there is an entry and
+               -- that Bool says whether a file exists.
+               let z = formatContents $ f y
+               case swap c of
+                    Just g ->
+                      let swapFile = g file in
+                          catchJust -- This contains a possible race condition.
+                            (\ e ->
+                              if isDoesNotExistError e then
+                                 Just () else
+                                 Nothing)
+                            (do h <- openFile swapFile ReadMode
+                                hClose h
+                                throw SwapInUse)
+                            (\ _ ->
+                              do writeFile swapFile z
+                                 -- Assume the swap exists on the same volume.
+                                 renameFile swapFile file)
+                    _ -> writeFile file z
+          Left e -> throw $ ContentError e
+
+-- This is deprecated.
+readContents :: Configuration -> IO (Map String String)
+readContents c =
+  do x <- readFile $ name c
+     _ <- evaluate $ length x
+     case parseContents x of
+          Right y -> return y
+          Left e -> throw $ ContentError e
+
+-- This is deprecated.
+writeContents :: Configuration -> Map String String -> IO ()
+writeContents c y =
+  case swap c of
+       Just f ->
+         -- Check swap file existence.
+         do writeFile (f $ name c) $ formatContents y
+            -- Assume swap exists on the same volume.
+            renameFile (f $ name c) (name c)
+       _ -> writeFile (name c) $ formatContents y
+
 data Configuration =
-  Configuration {name :: String, editor :: Maybe String, swap :: String -> String}
+  Configuration
+    {name :: String,
+     editor :: Maybe String,
+     swap :: Maybe (String -> String),
+     skip :: Maybe Int,
+     wrap :: Maybe Int}
   deriving Show
+
+defaultConfiguration :: Configuration
+defaultConfiguration =
+  Configuration
+    {name = "CONTENTS",
+     editor = Nothing,
+     swap = Just $ \ x -> "." ++ x ++ ".swap",
+     skip = Just 20,
+     wrap = Just 80}
 
 execute :: Configuration -> [Action] -> IO ()
 execute c = mapM_ $ executeOne c
 
-test :: Either CommandError [Action]
-test = parseActions ["make", "to", "add", "key", "value", "look", "key"]
+testc :: Either CommandError [Action]
+testc = parseActions ["make", "to", "add", "key", "value", "look", "key"]
 
-defaultConfiguration :: Configuration
-defaultConfiguration =
-  Configuration {name = "CONTENTS", editor = Just "vim", swap = \ x -> "." ++ x ++ ".swap"}
+testp :: IO (Either ParseError (Map String String))
+testp = parseContents <$> readFile (name defaultConfiguration)
+
+testf :: IO (Either ParseError String)
+testf = fmap formatContents <$> testp
+
+testq :: IO ()
+testq =
+  do x <- testf
+     case x of
+          Right y -> putStr y
+          Left e -> print e
 
 mainWith :: [String] -> IO ()
 mainWith xs =
   case parseActions xs of
-       Right x -> execute defaultConfiguration x
+       Right x ->
+         do c <- return defaultConfiguration -- Read here.
+            execute c x
        Left x -> print x
 
 main :: IO ()
